6ff5cfd1f753ffea10357485f91f8e88dc9d3c7c
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-02-17 19:13:17 +0300

    Merge work

---
+++ ConvertObjToFbxAndColorate/ConvertObjToFbxAndColorate/Converter.cs
@@ -0,0 +1,24 @@
+﻿using Aspose.ThreeD;
+using Aspose.ThreeD.Formats;
+using System.Collections.Generic;
+
+namespace ConvertObjToFbxAndColorate
+{
+    public class Converter
+    {
+
+        public static void ConverteFromObjToFbx(IEnumerable<string> fromPathCoollerction, string to)
+        {
+            var mainDocument = new Scene();
+            
+            foreach(var path in fromPathCoollerction )
+            {
+                var document = new Scene(path);
+                mainDocument.RootNode.AddChildNode(document.RootNode);
+
+            }  
+            var options = new FBXSaveOptions(FileFormat.FBX7500Binary);
+            mainDocument.Save(to, options);
+        }
+    }
+}
---
+++ ConvertObjToFbxAndColorate/ConvertObjToFbxAndColorate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace ConvertObjToFbxAndColorate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceFilesCollection = Directory.GetFiles(@"D:\Projects\aibolit-src\ObjToFbx\3D model from CT");
+            Converter.ConverteFromObjToFbx(
+                    sourceFilesCollection,
+                    "./test.fbx"
+                    );
+        }
+    }
+}
